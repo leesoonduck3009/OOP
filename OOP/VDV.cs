5a53f68b51fa04442ed81c1ae41898f7b038c04b
PNB309 <nguyen.phucbinh445@gmail.com>
2023-01-11 00:25:10 +0700

    Update 11/1/2022

---
+++ OOP/VDV.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    internal class VDV
+    {
+        int maSo;
+        string hoTen;
+        int tuoi;
+        int maSoDoi;
+        public TimeSpan TGChay;
+
+        public int MaSo { get => maSo; set => maSo = value; }
+        public string HoTen { get => hoTen; set => hoTen = value; }
+        public int Tuoi { get => tuoi; set => tuoi = value; }
+
+        public void NhapVDV()
+        {
+            int gio, phut, giay;
+            // Nhap MaSO
+            Console.WriteLine("Nhap ...");
+            // Nhap HoTen
+            // Nhap Tuoi
+            // Nhap gio chay
+            // Nhap phut chay
+            // Nhap gio giay chay
+            TGChay = new TimeSpan(gio, phut, giay);
+        }
+        public void XuatVDV()
+        {
+
+        }
+        public override string ToString()
+        {
+            return 
+        }
+
+    }
+}
